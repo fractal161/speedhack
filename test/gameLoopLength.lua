38f565d54772c797e88d13c35911314800d0196f
fractal161 <628snapdragon@gmail.com>
2022-01-23 21:15:53 -0500

    measures cycle counts of main loop

---
+++ test/gameLoopLength.lua
@@ -0,0 +1,23 @@
+﻿totalCycles = 29780
+gameLogicCycleStart = 0
+elapsedCycles = 0
+maxElapsedCycles = 0
+
+function start()
+  gameLogicCycleStart = emu.getState().cpu.cycleCount
+end
+
+function finish()
+  elapsedCycles = emu.getState().cpu.cycleCount - gameLogicCycleStart
+  if elapsedCycles > maxElapsedCycles then
+    maxElapsedCycles = elapsedCycles
+  end
+end
+
+function draw()
+  emu.drawString(9,9,maxElapsedCycles,0xFFFFFF,0x000000)
+end
+
+emu.addMemoryCallback(start, emu.memCallbackType.cpuExec, 0x81F9)
+emu.addMemoryCallback(finish, emu.memCallbackType.cpuExec, 0x8202)
+emu.addEventCallback(draw, emu.eventType.endFrame)
